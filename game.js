809ef6f582124fb7f9375de70fd28cabd23337d5
tuanbinh8 <tuanbinh1209@gmail.com>
2022-03-23 18:57:35 +0700

    update some code

--- game.js
+++ game.js
@@ -4,24 +4,24 @@ let player, playerName, playerHP, playerSpeed, playerJumpSpeed, playerNameDispla
 let enemy, enemyName, enemyHP, enemySpeed, enemyJumpSpeed, enemyNameDisplayer, enemyHPDisplayer, enemyDirection
 let timeLeft, timeLeftDisplayer
 let ground
+let circle, active
 
 function start() {
-    document.body.style.backgroundColor = 'black'
     document.getElementById('filter').style.display = 'none'
     document.getElementById('play-button').style.display = 'none'
     gameArea.start()
-    player = new Character('rect', 0, 200, 60, 60, 'red', 'player');
+    player = new Character('rect', 0, 200, 60, 60, 'red', true);
     do {
         playerName = prompt('Enter your name')
     }
     while (playerName == '' || !playerName);
-    playerNameDisplayer = new Component('text', 0, 0, '20px', 'Consolas', 'red')
+    playerNameDisplayer = new Component('text', 0, 0, '20px', 'Consolas', 'red', true)
     playerHP = 10
     playerSpeed = 5
     playerJumpSpeed = 6.5
-    playerHPDisplayer = new Component('text', 15, 45, '30px', 'Consolas', 'black')
+    playerHPDisplayer = new Component('text', 15, 45, '30px', 'Consolas', 'black', true)
     playerDirection = 'right'
-    enemy = new Character('rect', gameArea.canvas.width - 20, 200, 60, 60, 'black');
+    enemy = new Character('rect', gameArea.canvas.width - 20, 200, 60, 60, 'black', true);
     do {
         enemyName = prompt('Enter your enemy name')
     }
@@ -30,23 +30,25 @@ function start() {
         playerName += ' (player)'
         enemyName += ' (enemy)'
     }
-    enemyNameDisplayer = new Component('text', 0, 0, '20px', 'Consolas', 'black')
+    enemyNameDisplayer = new Component('text', 0, 0, '20px', 'Consolas', 'black', true)
     enemyHP = 10
     enemySpeed = 3
     enemyJumpSpeed = 4.5
-    enemyHPDisplayer = new Component('text', 15, 95, '30px', 'Consolas', 'black')
+    enemyHPDisplayer = new Component('text', 15, 95, '30px', 'Consolas', 'black', true)
     enemyDirection = 'left'
     timeLeft = 60
-    timeLeftDisplayer = new Component('text', gameArea.canvas.width - 250, 45, '30px', 'Consolas', 'black')
-    ground = new Component('rect', 0, gameArea.canvas.height - 120, gameArea.canvas.width, 120, 'green')
-    components.push(ground)
-    components.push(playerNameDisplayer)
-    components.push(playerHPDisplayer)
-    components.push(enemyNameDisplayer)
-    components.push(enemyHPDisplayer)
-    components.push(timeLeftDisplayer)
-    characters.push(player)
-    characters.push(enemy)
+    timeLeftDisplayer = new Component('text', gameArea.canvas.width - 250, 45, '30px', 'Consolas', 'black', true)
+    ground = new Component('rect', 0, gameArea.canvas.height - 120, gameArea.canvas.width, 120, 'green', true)
+    circle = new Component('circle', 0, 0, 10, undefined, 'rgba(0,0,0,1)', true)
+    active = true
+    addComponent(false, ground)
+    addComponent(false, playerNameDisplayer)
+    addComponent(false, playerHPDisplayer)
+    addComponent(false, enemyNameDisplayer)
+    addComponent(false, enemyHPDisplayer)
+    addComponent(false, timeLeftDisplayer)
+    addComponent(true, player)
+    addComponent(true, enemy)
 }
 
 let gameArea = {
@@ -59,10 +61,10 @@ let gameArea = {
         }, 1000);
         window.onkeydown = (event) => {
             gameArea.keys = (gameArea.keys || []);
-            gameArea.keys[event.keyCode] = (event.type == "keydown");
+            gameArea.keys[event.keyCode] = true
         }
         window.onkeyup = (event) => {
-            gameArea.keys[event.keyCode] = (event.type == "keydown");
+            gameArea.keys[event.keyCode] = false
         }
         window.addEventListener("visibilitychange", () => {
             if (timeLeft && playerHP && enemyHP) {
@@ -96,8 +98,20 @@ let gameArea = {
     },
 }
 
+function addComponent(isCharacter, component) {
+    components.push(component)
+    if (isCharacter)
+        characters.push(component)
+}
+function deleteComponent(isCharacter, component) {
+    if (components.indexOf(component) > -1) {
+        components.splice(components.indexOf(component), 1)
+        if (isCharacter)
+            characters.splice(characters.indexOf(component), 1)
+    }
+}
 class Component {
-    constructor(type, x, y, width, height, color) {
+    constructor(type, x, y, width, height, color, fill) {
         this.ctx = gameArea.ctx
         this.type = type
         this.x = x
@@ -105,6 +119,7 @@ class Component {
         this.width = width
         this.height = height
         this.color = color
+        this.fill = fill
         this.left = this.x;
         this.right = this.x + (this.width);
         this.top = this.y;
@@ -112,8 +127,12 @@ class Component {
     }
     draw() {
         if (this.type == 'rect') {
-            this.ctx.fillStyle = this.color;
-            this.ctx.fillRect(this.x, this.y, this.width, this.height);
+            this.ctx.strokeStyle = this.color
+            this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+            if (this.fill) {
+                this.ctx.fillStyle = this.color;
+                this.ctx.fillRect(this.x, this.y, this.width, this.height);
+            }
         }
         if (this.type == 'image') {
             let image = new Image()
@@ -133,43 +152,56 @@ class Component {
             this.ctx.fillStyle = this.color;
             this.ctx.fillText(this.text, this.x, this.y);
         }
+        if (this.type == 'circle') {
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.width, 0, 2 * Math.PI);
+            this.ctx.strokeStyle = this.color
+            this.ctx.stroke();
+            if (this.fill) {
+                this.ctx.fillStyle = this.color
+                this.ctx.fill();
+            }
+        }
     }
     clicked() {
-        let myleft = this.x;
-        let myright = this.x + (this.width);
-        let mytop = this.y;
-        let mybottom = this.y + (this.height);
-        let clicked = false;
-        if ((mybottom >= gameArea.mouseY) || (mytop <= gameArea.mouseY) || (myright >= gameArea.mouseX) || (myleft <= gameArea.mouseX)) {
-            clicked = true;
-        }
-        return clicked;
+        if (components.indexOf(this) > -1) {
+            let myleft = this.x;
+            let myright = this.x + (this.width);
+            let mytop = this.y;
+            let mybottom = this.y + (this.height);
+            let clicked = false;
+            if ((mybottom >= gameArea.mouseY) || (mytop <= gameArea.mouseY) || (myright >= gameArea.mouseX) || (myleft <= gameArea.mouseX)) {
+                clicked = true;
+            }
+            return clicked;
+        } else return false
     }
     touchWith(otherobj) {
-        let myleft = this.x;
-        let myright = this.x + (this.width);
-        let mytop = this.y;
-        let mybottom = this.y + (this.height);
-        let otherleft = otherobj.x;
-        let otherright = otherobj.x + (otherobj.width);
-        let othertop = otherobj.y;
-        let otherbottom = otherobj.y + (otherobj.height);
-        let touch = true;
-        if ((mybottom < othertop) ||
-            (mytop > otherbottom) ||
-            (myright < otherleft) ||
-            (myleft > otherright)) {
-            touch = false;
-        }
-        return touch;
+        if (components.indexOf(this) > -1 && components.indexOf(otherobj) > -1) {
+            let touch = true;
+            let myleft = this.x;
+            let myright = this.x + (this.width);
+            let mytop = this.y;
+            let mybottom = this.y + (this.height);
+            let otherleft = otherobj.x;
+            let otherright = otherobj.x + (otherobj.width);
+            let othertop = otherobj.y;
+            let otherbottom = otherobj.y + (otherobj.height);
+            if ((mybottom < othertop) ||
+                (mytop > otherbottom) ||
+                (myright < otherleft) ||
+                (myleft > otherright)) {
+                touch = false;
+            }
+            return touch;
+        } else return false
     }
 }
 
 class Character extends Component {
-    constructor(type, x, y, width, height, color, name) {
-        super(type, x, y, width, height, color)
+    constructor(type, x, y, width, height, color, fill) {
+        super(type, x, y, width, height, color, fill)
         this.ctx = gameArea.ctx
-        this.name = name
         this.speedX = 0
         this.speedY = 0
         this.gravity = 0.1;
@@ -217,7 +249,7 @@ class Character extends Component {
 }
 
 function drawBackground(type, color) {
-    let background = new Component(type, 0, 0, gameArea.canvas.width, gameArea.canvas.height, color)
+    let background = new Component(type, 0, 0, gameArea.canvas.width, gameArea.canvas.height, color, true)
     background.draw()
 }
 function playSound(src, loop, volume) {
@@ -247,14 +279,25 @@ function enemyAI() {
         enemy.moveDown(enemyJumpSpeed)
 }
 
+function resetStage() {
+    player.x = 0
+    player.y = 200
+    enemy.x = gameArea.canvas.width - 20
+    enemy.y = 200
+}
+
 function updateGameArea() {
     if (player.touchWith(enemy)) {
         playSound('diesound.mp3', false, 100)
-        player.x = 0
-        player.y = 200
-        enemy.x = gameArea.canvas.width - 20
-        enemy.y = 200
+        resetStage()
         playerHP--
+    }
+    if (enemy.touchWith(circle)) {
+        circle.width = 0
+        if (components.indexOf(circle) > -1)
+            deleteComponent(false, circle)
+        resetStage()
+        enemyHP--
     }
     player.speedX = 0;
     player.speedY = 0;
@@ -275,6 +318,30 @@ function updateGameArea() {
     if (keyDown(27)) {
         pause()
     }
+    if (keyDown(32)) {
+        if (active) {
+            active = false
+            setTimeout(() => {
+                active = true
+            }, 5000)
+            circle.x = player.x + player.width / 2
+            circle.y = player.y + player.height / 2
+            addComponent(false, circle)
+            let r = 10
+            let a = 1
+            growingCircle()
+            function growingCircle() {
+                r++
+                a -= 0.001
+                circle.width = r
+                circle.color = `rgba(0,0,0,${a})`
+                if (a > 0)
+                    requestAnimationFrame(growingCircle)
+                else
+                    deleteComponent(false, circle)
+            }
+        }
+    }
     timeLeftDisplayer.text = `time left: ${timeLeft}`
     playerNameDisplayer.text = playerName
     playerHPDisplayer.text = `${playerName} HP: ${playerHP}`
@@ -293,7 +360,6 @@ function updateGameArea() {
     })
     characters.map(character => {
         character.newPos()
-        character.draw()
     })
 }
 function keyDown(keyCode) {
